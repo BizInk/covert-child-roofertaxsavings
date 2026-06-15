826765f79b960addbf3a056c3c1f402dbdbd2bba
Jayden Major <jaydenmajor@gmail.com>
2026-06-15 16:10:59 +1000

    Updater & Editor Styles

--- functions.php
+++ functions.php
@@ -2,3 +2,26 @@
 
 add_editor_style();
 
+// Shortcode for yellow text
+add_shortcode('yellow-text', 'yellow_text_cb');
+function yellow_text_cb($atts, $content = null ){
+    return '<span class="yellow">' . $content . '</span>';
+}
+
+// Shortcode for line break
+add_shortcode('br', 'br_cb');
+function br_cb(){
+    return '<br>';
+}
+
+// Shortcode for current year
+add_shortcode('current-year', 'current_year_cb');
+function current_year_cb(){
+    return date('Y');
+}
+
+// Theme Updater
+require 'plugin-update-checker/plugin-update-checker.php';
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+$myUpdateChecker = PucFactory::buildUpdateChecker('https://github.com/BizInk/covert-child-roofertaxsavings',__FILE__,'covert-child-roofertaxsavings');
+$myUpdateChecker->setBranch('main');
---
+++ readme.txt
@@ -0,0 +1,20 @@
+=== Convert child Roof Tax Savings ===
+Contributors: Bizink, Jayden Major
+Tags: Bizink, Convert, Convert child, Roof Tax Savings
+Requires at least: 5.6
+Tested up to: 7.0
+Stable tag: 1.1
+Requires PHP: 7.4
+
+The Convert child Roof Tax Savings theme
+
+== Description ==
+The Convert child Roof Tax Savings theme
+
+== Changelog ==
+
+= 1.1 =
+* Updater
+
+= 1.0 =
+* First Realease
